fb69a480fb76456d9b8feccbb1ee09cbc2bacb98
anavb2309 <anavalenciaburbano@gmail.com>
2024-12-10 23:01:37 -0500

    spring_React2

--- src/main/java/com/interivalle/spring3/Spring3Application.java
+++ src/main/java/com/interivalle/spring3/Spring3Application.java
@@ -3,7 +3,10 @@ package com.interivalle.spring3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/*
+*
+*Cotizador Interivalle
+*/
 @SpringBootApplication
 public class Spring3Application {
 
